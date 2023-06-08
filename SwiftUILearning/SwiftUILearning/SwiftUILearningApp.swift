997bc1f7b87cdf3562a91de73bd006c05a7e734c
serenadonato <serena.donato@uala.com.ar>
2023-06-08 10:48:42 -0300

    initial commit

---
+++ SwiftUILearning/SwiftUILearning/SwiftUILearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningApp.swift
+//  SwiftUILearning
+//
+//  Created by Serena Donato on 08/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
