54c4f281c47ab7c1209866689449f1528c061dc4
LucasBenitez <lucas.benitez@uala.com.ar>
2023-08-25 00:02:26 -0300

    Add decorativePreviewComponent

--- SwiftUILearning/SwiftUILearning/SwiftUILearningApp.swift
+++ SwiftUILearning/SwiftUILearning/SwiftUILearningApp.swift
@@ -11,8 +11,7 @@ import SwiftUI
 struct SwiftUILearningApp: App {
     var body: some Scene {
         WindowGroup {
-            //ListRootView()
-            HeroView<HeroPresenter>.compose()
+            HeaderView()
         }
     }
 }
